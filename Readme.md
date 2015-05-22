221fd51de975466942b9fa5e7bb81a4466782228
Mudit Ameta <mudit.ameta@orbitz.com>
2015-05-22 15:42:43 -0500

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# gradle-tab-completion
+
+This script adds tab completion for gradle based on the current tasks configured in gradle.
+
+## Installation
---
+++ gradlew-tab-completion.bash
@@ -0,0 +1,30 @@
+# run this whenever there is a new task added or a task is removed from gradle
+setupTasksList() {
+    local tasks_file="$HOME/.gradleTasksList"
+
+    if [[ -x ./gradlew ]]; then
+        ./gradlew --no-color --quiet tasks | grep ' - ' | awk '{print $1}' | tr '\n' ' ' > $tasks_file
+    else
+        printf "Error: No gradlew in this folder. \nPlease run setupTasksList from a folder that contains a gradlew executable\n" 1>&2
+        return 1
+    fi
+}
+
+# gotta love bash
+_gradle() {
+    local tasks=''
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    local tasks_file="$HOME/.gradleTasksList"
+
+    if [[ ! -f $tasks_file ]]; then
+        setupTasksList
+    fi
+
+    tasks=$(cat $tasks_file)
+    COMPREPLY=( $(compgen -W "$tasks" -- $cur) )
+}
+
+# setup gradle commands to use _gradle completion function
+complete -F _gradle gradle
+complete -F _gradle gradlew
+complete -F _gradle ./gradlew
